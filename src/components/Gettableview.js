5a15c6cc6ea05a8e6225f6640bcb1fed9849c818
Abhishek Raj <deepclearning@gmail.com>
2020-02-28 23:02:14 +0530

    Initial commit

---
+++ src/components/Gettableview.js
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+import users from "../data/users.js";
+
+class Gettableview extends Component {
+    constructor(props){
+        super(props);
+        this.state = {            
+            users : users            
+        };
+    }
+
+    onSort(event, sortKey) {
+        const data = this.state.users;
+        data.sort((a,b) => a[sortKey].localeCompare(b[sortKey]))
+        this.setState({data})
+    }
+
+    render() {
+        const {users} = this.state;
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                    <th onClick={e=>this.onSort(e,"first_name")}>First Name</th>
+                    <th onClick={e=>this.onSort(e,"last_name")}>Last Name</th>
+                    <th onClick={e=>this.onSort(e,"company_name")}>Company Name</th>
+                    <th onClick={e=>this.onSort(e,"city")}>City</th>
+                    <th onClick={e=>this.onSort(e,"state")}>State</th>
+                    <th onClick={e=>this.onSort(e,"zip")}>Zip</th>
+                    <th onClick={e=>this.onSort(e,"email")}>Email</th>
+                    <th onClick={e=>this.onSort(e,"web")}>Web</th>
+                    <th onClick={e=>this.onSort(e,"id")}>Id</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {users.map( user => <tr key={user.id}>
+                        <td>{user.first_name} </td>
+                        <td>{user.last_name} </td>
+                        <td>{user.company_name} </td>
+                        <td>{user.city} </td>
+                        <td>{user.state} </td>
+                        <td>{user.zip} </td>
+                        <td>{user.email} </td>
+                        <td>{user.web} </td>
+                        <td>{user.id} </td>
+                    </tr>)}
+                </tbody>
+            </table>
+        
+        );
+    }
+} 
+export default Gettableview;
