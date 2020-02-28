d3650ff50fe1947632e85e714022179b63052e24
amit <gautamamits95@gmail.com>
2020-02-28 23:51:43 +0530

    table styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Gettableview from "./components/Gettableview";
+import Gettableview from "./components/Gettableview.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <Gettableview/>
     </div>
   );
