f3001954ec9508f2ae0d1b3576c4e2b5b8562690
Abhishek Raj <deepclearning@gmail.com>
2020-02-29 01:25:50 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter, Redirect, NavLink, Link } from 'react-router-dom';
+import {useHistory, useLocation, useRouteMatch, useParams} from 'react-router'
 import './App.css';
 import Gettableview from "./components/Gettableview.jsx";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <header className="App-header">
+        <NavLink activeClassName="active" to="/contacts">Datapeace</NavLink>
+      </header>
+      </BrowserRouter>
       <Gettableview/>
     </div>
   );
